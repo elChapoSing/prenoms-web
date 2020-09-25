045f24041610651109d08cbed374cb6f84a64698
Tristan <tpochat@gama.com>
2020-09-25 14:47:32 +0800

    adding libraries (jquery ui and spin) start on js code for website

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,32 @@
-
+let xNames = null;
+let xData = null;
 // ################################################
 // ############# INITIALIZATION ###################
 // ################################################
+let initializeCrossfilter = () => {
+
+}
+let loadData = (mode) => {
+    let url = "/prenoms/crossfilter/"+mode;
+    return $.ajax(url, {})
+};
+
+let go = () => {
+    console.log("go");
+    let spinner = new Spinner({
+        top: "50%",
+        left: "50%",
+        position: "floating"
+    }).spin(document.getElementById("spinner"));
+    Promise.all([loadData("names"),loadData("data")]).then((values) => {
+        console.log(values);
+        spinner.stop();
+    }).catch((err) => {
+        console.log(err);
+    })
+};
 
 $(function () {
+    $("#btn-go").button()
+        .click(go);
 });
