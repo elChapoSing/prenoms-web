11dca23279458a42c0fe1d5530d524260a1f28ff
Tristan <tpochat@gama.com>
2020-09-25 09:39:29 +0800

    test pg query

--- routes/prenoms.js
+++ routes/prenoms.js
@@ -5,26 +5,26 @@ const JSONStream = require('../dbs/db-pg/JSONStream');
 const router = new Router();
 module.exports = router;
 
-router.get('/crossfilter', (req, res, next) => {
-    let maxLineNumber = req.query.maxLineNumber;
+router.get('/crossfilter/data', (req, res, next) => {
+    db_pg.pool.connect((err, client, done) => {
+        if (err) {
+            throw err;
+        }
+        const query = new db_pg.QueryStream('select * from public.prenoms_dep');
+        const stream = client.query(query);
+        stream.on('end', done);
+        stream.pipe(JSONStream.stringify()).pipe(res);
+    });
+});
+
+router.get('/crossfilter/names', (req, res, next) => {
     let nano = db_couch.nano;
     let db = nano.use("prenoms");
     db.get("all_names").then((body) => {
-        let all_names = body.names;
-        console.log(body["_id"]);
-        db_pg.pool.connect((err, client, done) => {
-            if (err) {
-                throw err;
-            }
-            const query = new db_pg.QueryStream('select * from public.prenoms_dep limit $1', [maxLineNumber]);
-            const stream = client.query(query);
-            stream.on('end', done);
-            stream.pipe(JSONStream.stringify( '[\n', '\n,\n', '\n]\n', 0, all_names)).pipe(res);
-        });
+        res.send(body.names);
     }).catch((err) => {
         res.status(500).send(err);
     })
 });
-
 
 
