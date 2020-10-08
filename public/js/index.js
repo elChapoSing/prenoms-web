5210a38c7e7f15379265f33f517187fb512f4c1e
Tristan <tpochat@gama.com>
2020-10-08 19:54:39 +0800

    progress

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
 let xNames = null;
 let xData = null;
+let dimNames = {};
+let groupNames = {};
+let dimData = {};
+let groupData = {};
 // ################################################
 // ############# INITIALIZATION ###################
 // ################################################
@@ -8,11 +12,70 @@ let formatDashboard = () => {
 };
 
 let showDashboard = () => {
-
+    let seriesChart = (params) => {
+        let group = groupData["annee"]["sum"]
+        let theChart = new dc.SeriesChart("#nombre", "data")
+            .height(null)
+            .width(null)
+            .renderTitle(false)
+            .dimension(dimData["annee"])
+            .group(group)
+            .x(d3.scaleBand())
+            .elasticX(true)
+            .elasticY(true)
+            .brushOn(false)
+            .legend(new dc.HtmlLegend().container("#nombre-legend").horizontal(true).highlightSelected(true))
+            .seriesAccessor((d) => d.key[0])
+            .keyAccessor((d) => d.key[1])
+            .margins({left: 70, top: 10, bottom: 30, right: 50})
+            .on("preRedraw", function (chart) {
+                chart.rescale();
+            })
+            .on("preRender", function (chart) {
+                chart.rescale();
+            });
+        return theChart;
+    };
+    seriesChart();
+    dc.renderAll("data");
 };
 
 let initializeCrossfilter = (mode, data) => {
+    if (mode === "names") {
+        xNames = null;
+        xNames = crossfilter(data);
+    } else if (mode === "data") {
+        xData = null;
+        console.time("input xfilter");
+        xData = crossfilter(data);
+        console.timeEnd("input xfilter");
 
+        let reducerGenerator = () => {
+            return reductio()
+                .sum((d) => +d["nombre"]);
+        }
+
+        // let fields_dim = ["prenom", "annee2", "departement", "sexe"];
+        let fields_dim = ["departement"];
+        for (let field of fields_dim) {
+            console.time("Dimension : " + field);
+            dimData[field] = xData.dimension(field);
+            console.timeEnd("Dimension : " + field);
+        }
+        // console.time("Dimension : annee");
+        // dimData["annee"] = xData.dimension((d) => [d.prenom, d.annee]);
+        // console.timeEnd("Dimension : annee");
+
+        // let fields_group = ["prenom", "annee", "annee2", "departement", "sexe"];
+        let fields_group = ["departement"];
+        for (let field of fields_group) {
+            console.time("Group : " + field);
+            groupData[field] = {};
+            groupData[field]["sum"] = dimData[field].group();
+            reducerGenerator()(groupData[field]["sum"]);
+            console.timeEnd("Group : " + field);
+        }
+    }
 }
 
 let loadData = (mode) => {
@@ -29,7 +92,10 @@ let loadData = (mode) => {
                 download: true,
                 header: true,
                 fastMode: true,
+                dynamicTyping: true,
                 step: (row) => {
+                    // row.data["annee"] = parseInt(row.data["annee"]);
+                    // row.data["nombre"] = parseInt(row.data["nombre"]);
                     res.push(row.data);
                 },
                 complete: () => {
@@ -49,22 +115,239 @@ let loadData = (mode) => {
         });
 };
 
+let showMap = () => {
+    let url = "/public/geojson/departements.json";
+    $.ajax(url, {}).then((departementsJson) => {
+        let mapChart = new dc.GeoChoroplethChart("#carte");
+        mapChart.dimension(dimData["departement"])
+            .group(groupData["departement"]["sum"])
+            .colors(d3.scaleQuantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
+            .colorDomain([0, 10000])
+            .colorCalculator(function (d) {
+                return d ? usChart.colors()(d) : '#ccc';
+            })
+            .overlayGeoJson(departementsJson.features, "departement", function (d) {
+                return d.properties.code;
+            })
+            .projection(d3.geoMercator().center([2, 47]))
+            .valueAccessor(function (kv) {
+                console.log(kv);
+                return kv;
+            })
+            .title(function (d) {
+                return "Departement: " + d.key + "\nTotal Prenoms: " + d.value;
+            });
+    });
+
+}
+
 let go = () => {
     console.log("go");
     let spinner = new Spinner({}).spin(document.getElementById("spinner"));
-    Promise.all([loadData("names"), loadData("data")]).then((values) => {
-        console.log("data loaded");
-        initializeCrossfilter("names", values[0]);
-        initializeCrossfilter("data", values[1]);
-        showDashboard();
-        formatDashboard();
-        spinner.stop();
+    // Promise.all([loadData("names"), loadData("data")]).then((values) => {
+    //     console.log("data loaded");
+    //     console.time("initialize names");
+    //     initializeCrossfilter("names", values[0]);
+    //     console.timeEnd("initialize names");
+    //     console.time("initialize data");
+    //     initializeCrossfilter("data", values[1]);
+    //     console.timeEnd("initialize data");
+    //     console.time("show Dashboard");
+    //     showDashboard();
+    //     console.timeEnd("show Dashboard");
+    //     console.time("format Dashboard");
+    //     formatDashboard();
+    //     console.timeEnd("format Dashboard");
+    //     spinner.stop();
+    // }).catch((err) => {
+    //     console.log(err);
+    // })
+    spinner.stop();
+};
+
+let updateCount = () => {
+    $("#count-number").html("Loading...");
+    let hasFilter = false;
+    let percentileFilters = []
+    let trendFilters = []
+    let syllableFilters = []
+    let genderFilters = []
+    $("#percentile > div.grid-selected").each(function () {
+        let theSplit = this.id.split("!");
+        if (!["percentile", "decade"].includes(theSplit[1])) {
+            percentileFilters.push([theSplit[1], theSplit[2]]);
+        }
+    });
+    $("#trend > div.grid-selected").each(function () {
+        let theSplit = this.id.split("!");
+        if (!["trend", "decade"].includes(theSplit[1])) {
+            trendFilters.push([theSplit[1], theSplit[2]]);
+        }
+    });
+    $("#syllabe > div.syllabe-container-selected").each(function () {
+        syllableFilters.push(this.id.split("-")[1]);
+    });
+    $("#sexe > div.gender-container-selected").each(function () {
+        genderFilters.push(this.id.split("-")[1]);
+    });
+    //get the count
+    let url = "/prenoms/filters"
+    let filtersData = {
+        isCount: true,
+    };
+    let filters = [...percentileFilters,...trendFilters,...syllableFilters,...genderFilters];
+    if (filters.length>0) {
+        filtersData.filters = filters;
+    }
+    $.ajax(url, {
+        data: JSON.stringify(filtersData),
+        headers: {"Content-Type":"application/json"},
+        method: "POST",
+    }).then((body) => {
+        $("#count-number").html(body.count);
     }).catch((err) => {
         console.log(err);
+        $("#count-number").html("Blah!");
     })
+}
+
+let transfoGenerator = (isChecked, className, selectedClassName) => {
+    if (isChecked) {
+        return (x) => x.removeClass(className).addClass(selectedClassName);
+    } else {
+        return (x) => x.removeClass(selectedClassName).addClass(className);
+    }
+}
+
+let toggleSelect = (checkbox, perimName, isRow, isCol) => {
+
+    let transfo = transfoGenerator(checkbox.checked, "grid-unselected", "grid-selected");
+    let source_div = checkbox.id.split("!").slice(1).join("!");
+    transfo($("#" + $.escapeSelector(source_div)));
+
+    if (isRow) {
+        // percentile!decade!1900 !> percentile!x!1900
+        let decade = checkbox.id.split("!")[3];
+        let re = new RegExp(perimName + "!-?[0-9]+!" + decade, "g");
+        $("#" + perimName + " > div").map(function () {
+            if (this.id.match(re)) {
+                transfo($(this));
+                $("#" + $.escapeSelector("toggle!" + this.id)).prop("checked", checkbox.checked);
+            }
+        });
+    } else if (isCol) {
+        //percentile!percentile!0 !> percentile!0!x
+        let percentile = checkbox.id.split("!")[3];
+        let re = new RegExp(perimName + "!" + percentile + "![0-9]{4}", "g");
+        $("#" + perimName + " > div").map(function () {
+            if (this.id.match(re)) {
+                transfo($(this));
+                $("#" + $.escapeSelector("toggle!" + this.id)).prop("checked", checkbox.checked);
+            }
+        })
+    } else {
+        let percentile = checkbox.id.split("!")[2];
+        let decade = checkbox.id.split("!")[3];
+        if (checkbox.checked) {
+            // verify if it completes a rwo/column to activate header
+            let reDecade = new RegExp(perimName + "!-?[0-9]+!" + decade, "g");
+            let rePercentile = new RegExp(perimName + "!" + percentile + "![0-9]{4}", "g");
+            let testRow = [];
+            $("#" + perimName + " > div").filter(function () {
+                return this.id.match(reDecade);
+            }).each(function () {
+                testRow.push($("#" + $.escapeSelector("toggle!" + this.id)).prop("checked"));
+            })
+            if (testRow.every(x => x)) {
+                $("#" + $.escapeSelector("toggle!" + perimName + "!decade!" + decade)).prop("checked", true);
+                transfo($("#" + $.escapeSelector(perimName + "!decade!" + decade)));
+            }
+            let testCol = [];
+            $("#" + perimName + " > div").filter(function () {
+                return this.id.match(rePercentile);
+            }).each(function () {
+                testCol.push($("#" + $.escapeSelector("toggle!" + this.id)).prop("checked"));
+            })
+            if (testCol.every(x => x)) {
+                $("#" + $.escapeSelector("toggle!" + perimName + "!percentile!" + percentile)).prop("checked", false);
+                transfo($("#" + $.escapeSelector(perimName + "!percentile!" + percentile)));
+            }
+        } else {
+            // header
+            // percentile!x!1900 !> percentile!percentile!x
+            $("#" + $.escapeSelector("toggle!" + perimName + "!percentile!" + percentile)).prop("checked", false);
+            transfo($("#" + $.escapeSelector(perimName + "!percentile!" + percentile)));
+            //row title
+            // percentile!0!x !> percentile!decade!x
+            $("#" + $.escapeSelector("toggle!" + perimName + "!decade!" + decade)).prop("checked", false);
+            transfo($("#" + $.escapeSelector(perimName + "!decade!" + decade)));
+        }
+    }
+    updateCount();
 };
+
+let toggleGender = (checkbox) => {
+    let gender = checkbox.id.split("-")[1];
+    transfoGenerator(checkbox.checked, "gender-container", "gender-container-selected")($("#sexe-" + $.escapeSelector(gender)));
+    updateCount();
+};
+
+let toggleSyllabes = (checkbox) => {
+    let number = checkbox.id.split("-")[2];
+    transfoGenerator(checkbox.checked, "syllabe-container", "syllabe-container-selected")($("#syllabe-" + $.escapeSelector(number)));
+    updateCount();
+};
+
+let generateGrid = (perimName) => {
+    let decades = Array.from(Array(12), (_, x) => 1900 + 10 * x);
+    let variables = [];
+    let step = 0;
+    if (perimName === "percentile") {
+        step = 25;
+        variables = Array.from(Array(4), (_, x) => step * x);
+    } else if (perimName === "trend") {
+        step = 50;
+        variables = Array.from(Array(5), (_, x) => -100 + step * x);
+    }
+    $("#" + perimName).append('<div id="' + perimName + '!decade!empty"></div>');
+    for (let decade of decades) {
+        $("#" + perimName).append(
+            '<div id="' + perimName + '!decade!' + decade + '" class="grid-unselected">\n' +
+            '<label for="toggle!' + perimName + '!decade!' + decade + '">' + decade + '</label>\n' +
+            '</div>');
+        $("#checkboxes").append(
+            '<input type="checkbox" id="toggle!' + perimName + '!decade!' + decade + '" onclick="toggleSelect(this,\'' + perimName + '\',true,false)">'
+        );
+    }
+    for (let variable of variables) {
+        $("#" + perimName).append(
+            '<div id="' + perimName + '!percentile!' + variable + '"  class="grid-unselected">\n' +
+            '<label for="toggle!' + perimName + '!percentile!' + variable + '">' + variable + '!' + (variable + step) + '</label>\n' +
+            '</div>');
+        $("#checkboxes").append(
+            '<input type="checkbox" id="toggle!' + perimName + '!percentile!' + variable + '" onclick="toggleSelect(this,\'' + perimName + '\',false,true)">'
+        );
+        for (let decade of decades) {
+            $("#" + perimName).append(
+                '<div id="' + perimName + '!' + variable + '!' + decade + '" class="grid-unselected">\n' +
+                '<label for="toggle!' + perimName + '!' + variable + '!' + decade + '">' + variable + '!' + decade + '</label>\n' +
+                '</div>'
+            );
+            $("#checkboxes").append(
+                '<input type="checkbox" id="toggle!' + perimName + '!' + variable + '!' + decade + '" onclick="toggleSelect(this,\'' + perimName + '\',false,false)">'
+            );
+        }
+    }
+
+}
 
 $(function () {
     $("#btn-go").button()
         .click(go);
+
+    // generate the grids for trends and percentiles
+    generateGrid("percentile");
+    generateGrid("trend");
+    showMap();
+    updateCount();
 });
--- routes/prenoms.js
+++ routes/prenoms.js
@@ -6,13 +6,14 @@ const parse = require('csv-parse');
 const router = new Router();
 const fs = require('fs');
 const path = require('path')
+const Promise = require("bluebird");
+const axios = require('axios');
 module.exports = router;
 
 router.get('/crossfilter/data', (req, res, next) => {
     let stream = fs.createReadStream(path.resolve("./public/data/data.csv"), "utf-8");
     stream.pipe(res);
 });
-
 
 router.get('/crossfilter/data_pg', (req, res, next) => {
     db_pg.pool.connect((err, client, done) => {
@@ -37,4 +38,59 @@ router.get('/crossfilter/names', (req, res, next) => {
     })
 });
 
+router.get('/filters/:type/:decade/:number', (req, res, next) => {
+    let nano = db_couch.nano;
+    let db = nano.use("prenoms");
+    db.view("decades", "decades_" + req.params.type, {
+        "start_key": [req.params.decade, parseInt(req.params.number)],
+        "end_key": [req.params.decade, parseInt(req.params.number) + 1],
+        "reduce": false,
+    }).then((body) => {
+        res.send(body.rows.map(x => x.value));
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+});
 
+
+router.post('/filters', (req, res, next) => {
+    let isCount = req.body.isCount;
+    if (req.body.hasOwnProperty("filters")) {
+        let dataPromise = req.body.filters;
+        Promise
+            .map(dataPromise, (x) => {
+                return axios.get("/" + ['prenoms', 'filters', x.type, x.decade, x.number].join("/"), {
+                    proxy: {
+                        host: '127.0.0.1',
+                        port: 4000,
+                    },
+                }).then(body => body.data).catch(err => err);
+            }, {concurrency: 20})
+            .then((values) => {
+                let arr = values.flat(1).sort().filter((v, i, a) => a.indexOf(v) === i);
+                if (isCount) {
+                    res.send({count: arr.length});
+                } else {
+                    res.send(arr);
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+    } else {
+        let nano = db_couch.nano;
+        let db = nano.use("prenoms");
+        db.view("summaries", "all_names", {
+            "group_level": isCount ? 0:1,
+        }).then((body) => {
+            if (isCount) {
+                res.send({count:body.rows[0].value});
+            } else {
+                res.send(body.rows.map(x => x.key));
+            }
+        }).catch((err) => {
+            res.status(500).send(err);
+        })
+    }
+
+});
